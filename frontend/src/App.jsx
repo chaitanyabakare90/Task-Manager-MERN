c63ef4e59498a5e902ffcf915566c5e3ea11cbda
chaitanyabakare90 <chaitanyabakare7@gmail.com>
2026-07-05 22:43:28 +0530

    Logout Function Implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,39 @@
 import AddTask from "./components/AddTask"
 import List from "./components/List"
 import NavBar from "./components/NavBar"
-import {BrowserRouter,Route,Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import UpdateTask from "./components/UpdateTask"
 import SignUp from "./components/SignUp"
 import Login from "./components/Login"
+import ProtectedRoute from "./components/ProtectedRoute"
 
 
 function App() {
   return (
-      <>
-     
-       <NavBar></NavBar>
-       <Routes>
-          <Route path="/list" element={<List></List>}></Route>
-          <Route path="/add" element={<AddTask></AddTask>}></Route>
-          <Route path="/signup" element={<SignUp></SignUp>}></Route>
-          <Route path="/login" element={<Login></Login>}></Route>
-          <Route path="/update/:id" element={<UpdateTask></UpdateTask>}></Route>
-       </Routes>
-      
-      </>
-       
-      
+    <>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/list"
+          element={<ProtectedRoute>
+            <List />
+          </ProtectedRoute>}></Route>
+        <Route path="/add"
+          element={<ProtectedRoute>
+            <AddTask/>
+          </ProtectedRoute>}></Route>
+        <Route path="/update/:id" 
+          element={<ProtectedRoute>
+          <UpdateTask/>
+        </ProtectedRoute>}>
+        </Route>    
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        
+      </Routes>
+
+    </>
+
+
   )
 }
 
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,15 +1,46 @@
-import { Link } from "react-router";
+import { Link,useNavigate } from "react-router";
 import "../style/NavBar.css"
 
 export default function NavBar(){
+    const token  = localStorage.getItem("token");
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/login");
+    };
+
     return (
         <nav className="NavBar">
             <div className="logo"><h2>To Do App</h2></div>
             <ul>
                 <li><Link className="nav-links" to="/list">List</Link></li>
                 <li><Link className="nav-links" to="/add">Add Task</Link></li>
-                <li><Link className="nav-links" to="/signup">Signup</Link></li>
-                <li><Link className="nav-links" to="/login">Login</Link></li>
+                {token ? (
+                    <li>
+                        <Link
+                            className="nav-links"
+                            to="/login"
+                            onClick={handleLogout}
+                        >
+                            Logout
+                        </Link>
+                    </li>
+                ) : (
+                    <>
+                        <li>
+                            <Link className="nav-links" to="/signup">
+                                Signup
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link className="nav-links" to="/login">
+                                Login
+                            </Link>
+                        </li>
+                    </>
+                )}
             </ul>
         </nav>
     )
